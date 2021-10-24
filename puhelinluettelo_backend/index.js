8eea9914e519edfa5975b6e521a9394623559d46
Toni Asikainen <toniasikainen@outlook.com>
2021-10-24 13:41:43 +0300

    initial commit

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -127,8 +127,7 @@ app.get('/api/info',(req,res) => {
                 ${new Date().toString()} </p>
             </div>`)
 })
-
-const PORT = 3001
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
