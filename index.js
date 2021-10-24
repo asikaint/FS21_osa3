df14471ad713e47ba148d03f3d151319131cb926
Toni Asikainen <toniasikainen@outlook.com>
2021-10-24 15:20:02 +0300

    uibuild

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
+
 // app.use(morgan('tiny'))
 
 morgan.token('bodyJSON', req => JSON.stringify(req.body || {}));
